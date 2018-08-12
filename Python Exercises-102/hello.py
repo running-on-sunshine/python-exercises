487578d14196cfe5cdbdfa94fa6a65d69c6fcf2f
Liz Khuu <khuu.liz@gmail.com>
2018-08-12 10:25:19 -0400

    Corrected for instruction typo

--- Python Exercises-102/hello.py
+++ Python Exercises-102/hello.py
@@ -9,7 +9,7 @@
 #  ======================================================================= #
 
 # Prompt the user for their name using the raw_input function.
-# Upon receiving his their, you will say hello to them. 
+# Upon receiving their name, you will say hello to them. 
 
 name = raw_input('What is your name? ')
 message = 'Hello, ' + name + '!'
